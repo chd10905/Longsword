33cc582b91d5cef0535fa1306a18216e2589f9f0
chd10905 <cdh10905@ggh.goe.go.kr>
2025-04-15 08:13:54 +0900

    Update Rooms.gd

--- LongSword/Rooms.gd
+++ LongSword/Rooms.gd
@@ -22,7 +22,7 @@ func _spawn_rooms() -> void:
 	for i in num_levels:
 		var room: Node2D
 		
-		if i == 0:
+		if i == 0: # 시작 방이ㅣ 경우
 			room = SPAWN_ROOMS[randi() % SPAWN_ROOMS.size()].instance()
 			player.position = room.get_node("PlayerSpawnPos").position
 		else:
@@ -30,17 +30,18 @@ func _spawn_rooms() -> void:
 				room = END_ROOMS[randi() % END_ROOMS.size()].instance()
 			else:
 				room = INTERMEDIATE_ROOMS[randi() % INTERMEDIATE_ROOMS.size()].instance()
-			var previous_room_tilemap : TileMap = previous_room.get_node("TileMap")
-			var previous_room_door: StaticBody2D = previous_room.get_node("Doors/Door")
-			var exit_tile_pos: Vector2 = previous_room_tilemap.world_to_map(previous_room_door.position) + Vector2.UP * 2
+			var previous_room_tilemap : TileMap = previous_room.get_node("TileMap") # 이전방의 타일을 가져옴
+			var previous_room_door: StaticBody2D = previous_room.get_node("Doors/Door")# 이전 방의 문
+			var exit_tile_pos: Vector2 = previous_room_tilemap.world_to_map(previous_room_door.position) + Vector2.UP * 2 #문위치에서 타일맵 상의 출구 위치 계산
 			
-			var corridor_height: int = randi() % 5 + 2
-			for y in corridor_height:
+			var corridor_height: int = randi() % 5 + 2 # 복도 길
+			for y in corridor_height:# 복도 생성
 				previous_room_tilemap.set_cellv(exit_tile_pos+Vector2(-2,-y),LEFT_WALL_INDEX)
 				previous_room_tilemap.set_cellv(exit_tile_pos+Vector2(-1,-y),FLOOR_TILE_INDEX)
 				previous_room_tilemap.set_cellv(exit_tile_pos+Vector2(0,-y),FLOOR_TILE_INDEX)
 				previous_room_tilemap.set_cellv(exit_tile_pos+Vector2(1,-y),RIGHT_WALL_INDEX)
 			var room_tilemap: TileMap = room.get_node("TileMap")
+			# 방위치 계산 이전 방의 충구와 복도의 길이를 기준
 			room.position = previous_room_door.global_position + Vector2.UP * room_tilemap.get_used_rect().size.y * TILE_SIZE + Vector2.UP * (1 + corridor_height) * TILE_SIZE + Vector2.LEFT * room_tilemap.world_to_map(room.get_node("Entrance/Position2D2").position).x * TILE_SIZE
-		add_child(room)
-		previous_room = room
+		add_child(room) # 방 자식노드 추가
+		previous_room = room # 이전방이 현재 방의로 업데이트
