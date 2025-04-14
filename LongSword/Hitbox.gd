cf99b532d28027ffd1fd772d13f474b8efa91fdb
chd10905 <cdh10905@ggh.goe.go.kr>
2025-04-15 08:08:44 +0900

    Update Hitbox.gd

--- LongSword/Hitbox.gd
+++ LongSword/Hitbox.gd
@@ -7,7 +7,7 @@ export(int) var knockback_force: int = 300
 
 # var body_inside: bool = false
 
-onready var collision_shape: CollisionShape2D = get_child(0)
+onready var collision_shape: CollisionShape2D = get_child(0) #충돌 모양 저장
 #onready var timer: Timer = Timer.new()
 
 func _init()->void:
@@ -23,14 +23,14 @@ func _on_body_entered(body: PhysicsBody2D) -> void:
 	#while body_inside:
 	#	_collide(body)
 	#	yield(timer,"timeout")
-	_collide(body)
+	_collide(body) # 히트
 func _on_body_exited(_body: KinematicBody2D) -> void:
 	#body_inside = false
 	#timer.stop()
 	pass # 주석 해제시 이부분 삭제
 	
-func _collide(body: KinematicBody2D) -> void:
+func _collide(body: KinematicBody2D) -> void: 
 	if body == null or not body.has_method("take_damage"):
-		queue_free()
+		queue_free() # body 가 없거나 데미지 함수가 없으면 삭제
 	else:
 		body.take_damage(damage, knockback_direction,knockback_force)
